b19ff620ac541997104d06e1c91f9dffe3c5b655
Zircoz <dixit.setu@gmail.com>
2018-08-15 17:05:44 +0530

    Turned them to html/css for further dissection

--- src/register.php
+++ src/register.php
@@ -2,20 +2,20 @@
    require "vendor/autoload.php";
    $client = new MongoDB\Client;
    $db = $client->db_register->users;
-	
+
   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$ins = $db->insertOne([
 		'username' => $_POST['username'],
 		'email' => $_POST['email'],
 		'phone' => $_POST['phone'],
 		'pass' => $_POST['pass'],
-		
+
 	]);
-	
-	
+
+
    	//var_dump($ins->getInsertedId());
-  }   
-	
+  }
+
 ?>
 
 <!DOCTYPE html>
@@ -26,14 +26,14 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<script rel="jquery" type="text/javascript" src="myjs.js"></script>    
+		<script rel="jquery" type="text/javascript" src="myjs.js"></script>
 <!--		<link rel="stylesheet" type="text/css" href="logincss.css">-->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
             body{
                 background: url(s2.jpg) no-repeat center center fixed;
-                
+
                 -webkit-background-size: cover;
                 -moz-background-size: cover;
                 -o-background-size: cover;
@@ -50,17 +50,17 @@
                 border-width: thin medium thick 10px;
                 box-shadow: 5px 10px 20px ;
                 font-family:Arial, Helvetica, sans-serif;
-                
+
             }
 
             h1{
                font-family:Ubuntu-Regular;
-               
-               
+
+
             }
             .ihold{
                <!-- background: url(student.jpg);
-                
+
                 background-size: cover; -->
             }
             p{
@@ -79,7 +79,7 @@
                text-transform: uppercase;
 
                display: block;
-             }  
+             }
              .credentials{
                  font-family: monospace-Bold ;
                  width: 100%;
@@ -90,20 +90,20 @@
                  padding: 10px 0px;
                  background: transparent;
                  font-size: 15px;
-             } 
+             }
              .for-ep{
                  font-family: monospace-Bold;
                  font-weight: 900;
                  font-size: 15px;
                  color: black;
                  width: 100%;
-                  
+
              }
              .for-button{
                  font-family: monospace;
                  background-color: transparent;
                  border: 1px solid transparent;
-             
+
                  padding: 15px 32px;
                  text-align: center;
                  text-decoration: none;
@@ -112,7 +112,7 @@
                  cursor: pointer;
                  float: center;
                  margin:30px 50px ;
-             } 
+             }
              .fa {
                  padding: 15px;
                  font-size: 20px;
@@ -130,7 +130,7 @@
                   background: #3B5998;
                   color: white;
              }
-            
+
              .fa-google {
                   background: #dd4b39;
                   color: white;
@@ -188,7 +188,7 @@ li a:hover:not(.active) {
 .submit-btn:hover{
     background-color: #333;
     }
-    
+
     </style>
 
 </head>
@@ -210,7 +210,7 @@ li a:hover:not(.active) {
 <form method="POST" enctype="multipart/form-data" action="">
 <!--<span class="dspan">Student Login</span>-->
 <h2 style="text-align:center;"><?php
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+	if ($_SERVER['REQUEST_METHOD'] == 'POST')
 		printf("Registered %d User!", $ins->getInsertedCount());
 ?></h2>
 <div class="log ihold">
@@ -228,7 +228,7 @@ li a:hover:not(.active) {
 <input class="credentials " type="password" id="pwd" name="pwd" placeholder="Re-Enter Password"><br/><br/>
 
     <button class="submit-btn" type="submit"  name="login" onclick="return validate();" >SignUp</button>
-<br/><br/><font style="color:orange">Or&#160 </font><a href="signup.html">Sign Up&#160 </a><font style="color:orange">Using :&#160 (If already a user&#160<a href="login.php">Login</a>)</font><br/> 
+<br/><br/><font style="color:orange">Or&#160 </font><a href="signup.html">Sign Up&#160 </a><font style="color:orange">Using :&#160 (If already a user&#160<a href="login.php">Login</a>)</font><br/>
 <a href="#" class="fb btn">
     <i class="fa fa-facebook fa-fw"></i> Login with Facebook
 </a>
